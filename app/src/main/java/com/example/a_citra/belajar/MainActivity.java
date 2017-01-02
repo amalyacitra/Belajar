b29901b9ddfe0d30abd7d97a46bda1b778709b6b
Amalya Citra <amalyacitra@gmail.com>
2017-01-02 18:25:36 +0700

    date picker di lapor sudah jalan

--- app/src/main/java/com/example/a_citra/belajar/MainActivity.java
+++ app/src/main/java/com/example/a_citra/belajar/MainActivity.java
@@ -32,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
-        Log.d("Citra", "sampe sini");
         Toolbar toolbar = (Toolbar) findViewById(R.id.appbar);
         toolbar.setTitle("Belajar");
 
@@ -66,7 +65,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void pilihKeluar(MenuItem item) {
-        //Toast.makeText(this, "Hello World", Toast.LENGTH_LONG).show();
         item.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
 
             @Override
--- app/src/main/java/com/example/a_citra/belajar/tab_fragment_lapor.java
+++ app/src/main/java/com/example/a_citra/belajar/tab_fragment_lapor.java
@@ -1,15 +1,70 @@
 package com.example.a_citra.belajar;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+
+import java.util.Calendar;
 
 
-public class tab_fragment_lapor extends Fragment {
+public class tab_fragment_lapor extends Fragment implements DatePickerDialog.OnDateSetListener {
+
+    private TextView dateTextView;
+
+
+
+    public tab_fragment_lapor() {
+
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.activity_tab_fragment_lapor, container, false);
+
+        dateTextView = (TextView) rootView.findViewById(R.id.date_textview);
+        Button dateButton = (Button) rootView.findViewById(R.id.date_button);
+
+        dateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar now = Calendar.getInstance();
+                DatePickerDialog dpd = DatePickerDialog.newInstance(
+                        tab_fragment_lapor.this,
+                        now.get(Calendar.YEAR),
+                        now.get(Calendar.MONTH),
+                        now.get(Calendar.DAY_OF_MONTH)
+
+                );
+                dpd.show(getActivity().getFragmentManager(), "Datepickerdialog");
+            }
+        });
+
+
+
         return rootView;
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        DatePickerDialog dpd = (DatePickerDialog) getActivity().getFragmentManager().findFragmentByTag("Datepickerdialog");
+        if(dpd != null) dpd.setOnDateSetListener(this);
+    }
+
+    @Override
+    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+        String date = dayOfMonth+"/"+(++monthOfYear)+"/"+year;
+        dateTextView.setText(date);
+    }
+
+
 }
